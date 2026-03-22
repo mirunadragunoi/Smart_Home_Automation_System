6829f44c5e634a3bde39aeb9bb4ca9a7e71915b5
Dragunoi Miruna <mirudragunoi@gmail.com>
2026-03-22 22:38:27 +0200

    implementare house & room

--- src/model/House.java
+++ src/model/House.java
@@ -1,4 +1,62 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class House {
+    private int id;
+    private String adresa;
+    private User owner;
+    private List<Room> rooms;
+
+    public House() {
+        this.rooms = new ArrayList<>();
+    }
+
+    public House (int id, String adresa, User owner) {
+        this.id = id;
+        this.adresa = adresa;
+        this.owner = owner;
+        this.rooms = new ArrayList<>();
+    }
+
+    // geteri
+    public int getId() {
+        return id;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public List<Room> getRooms() {
+        return rooms;
+    }
+
+    // setteri
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public void setRooms(List<Room> rooms) {
+        this.rooms = rooms;
+    }
+
+    @Override
+    public String toString() {
+        return "* HOUSE * -> {id = " + id + ", adresa = " + adresa + ", owner = " + owner.getNume()
+                + ", rooms = " + rooms.size() + " }";
+    }
 }
--- src/model/Room.java
+++ src/model/Room.java
@@ -1,4 +1,78 @@
 package model;
 
+import model.device.Device;
+import model.senzor.Senzor;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Room {
+    private int id;
+    private String nume;
+    private String type;
+    private List<Device> devices;
+    private Set<Senzor> senzori;
+
+    public Room() {
+        this.devices = new ArrayList<>();
+        this.senzori = new HashSet<>();
+    }
+
+    public Room(int id, String nume, String type) {
+        this.id = id;
+        this.nume = nume;
+        this.type = type;
+        this.devices = new ArrayList<>();
+        this.senzori = new HashSet<>();
+    }
+
+    // getteri
+    public int getId() {
+        return id;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public List<Device> getDevices() {
+        return devices;
+    }
+
+    public Set<Senzor> getSenzori() {
+        return senzori;
+    }
+
+    // setteri
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDevices(List<Device> devices) {
+        this.devices = devices;
+    }
+
+    public void setSenzori(Set<Senzor> senzori) {
+        this.senzori = senzori;
+    }
+
+    @Override
+    public String toString() {
+        return " * ROOM * -> { id = " + id + ", nume = " + nume + ", type = " + type +
+                ", devices = " + devices.size() + ", senzori = " + senzori.size() + " } ";
+    }
 }
